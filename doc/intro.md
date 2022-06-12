495138733e7a2677463725030c3db96105481fab
justNeto <continental.lelgg.2b@gmail.com>
2022-06-11 19:04:15 -0500

    First commit of the new repository

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to vending-machine-business
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/vending_machine_business/core_test.clj
@@ -0,0 +1,7 @@
+(ns vending-machine-business.core-test
+  (:require [clojure.test :refer :all]
+            [vending-machine-business.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
